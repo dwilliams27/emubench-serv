cc7de5e5805b25f0a4f37cb58d565889314e7038
Farmerbilly27 <dmw2796@gmail.com>
2025-04-26 20:54:52 -0500

    minor improvements

--- src/models.ts
+++ src/models.ts
@@ -18,16 +18,10 @@ interface StickPosition {
   y: number; // 0-255, center at 128
 }
 
-interface TriggerValues {
-  l: number; // 0-255
-  r: number; // 0-255
-}
-
 export interface IPCControllerInputRequest {
   connected: boolean;
   buttons?: Partial<Buttons>;
   mainStick?: StickPosition;
   cStick?: StickPosition;
-  triggers?: Partial<TriggerValues>;
   frames: number;
 }
